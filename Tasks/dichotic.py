6d58c46794601b6fe2ceedc39ecea0efca9f0d58
TomerOz <tomer.oz22@gmail.com>
2020-01-28 10:03:11 +0200

    dichotic problem

--- Tasks/dichotic.py
+++ Tasks/dichotic.py
@@ -21,8 +21,43 @@ class DichoticOneBack(object):
 		self.exp = exp
 		self.data_manager = data_manager
 		
+		self.dtd = DichoticTaskData(dichotic_data_manager, data_manager, gui)
 		
-		# TEMP
+	def _create_task_main_frame(self):
+		'''creating main frame (GUI) and lable to present fixration stimuli'''
+		self.exp.create_frame(MAIN_FRAME, background_color=BACKGROUND_COLOR)
+		self.exp.create_label(FIXATION_LABEL, MAIN_FRAME, label_text=FIXATION_STIMULI, label_font=FIXATION_FONT, label_bg=BACKGROUND_COLOR, label_fg=FOREGROUND_COLOR)
+		self.exp.display_frame(MAIN_FRAME,[FIXATION_LABEL])
+
+	def _play2tracks(self, event=None):
+		'''activating tow audio channels'''
+		try_sound = self.data_manager.sentence_inittial_path + '\\' + self.data_manager.sentences[0].file_path
+		pg.mixer.init(frequency=44000, size=-16,channels=2, buffer=4096)
+		
+		sound0 = pg.mixer.Sound(try_sound)
+		
+		channel0 = pg.mixer.Channel(0)
+		channel1 = pg.mixer.Channel(1)
+		
+		# Play the sound (that will reset the volume to the default).
+		channel0.play(sound0)
+		
+		# Now change the volume of the specific speakers.
+		# The first argument is the volume of the left speaker and
+		# the second argument is the volume of the right speaker.
+		channel0.set_volume(1.0, 0.0)
+		
+		def x():
+			channel1.set_volume(0.0, 1.0)
+			channel1.play(sound0)
+		self.gui.after(500, x)
+		
+
+class DichoticTaskData(object):
+	def __init__(self, dichotic_data_manager, data_manager, gui):
+		
+		self.gui = gui
+		self.data_manager = data_manager
 		self.block = 0
 		self.chunk = 1
 		self.valence = "neg"
@@ -31,17 +66,12 @@ class DichoticOneBack(object):
 		self.trial = 0
 		self.current_neu_sentence = self.dichotic_data_manager.blocks_dicts[0][self.chunk]["neu"][self.trial].sentence 
 		self.current_neg_sentence = self.dichotic_data_manager.blocks_dicts[0][self.chunk][self.valence][self.trial].sentence 
-		## END TEMP
-	
-	def _create_task_main_frame(self):
-		'''creating main frame (GUI) and lable to present fixration stimuli'''
-		self.exp.create_frame(MAIN_FRAME, background_color=BACKGROUND_COLOR)
-		self.exp.create_label(FIXATION_LABEL, MAIN_FRAME, label_text=FIXATION_STIMULI, label_font=FIXATION_FONT, label_bg=BACKGROUND_COLOR, label_fg=FOREGROUND_COLOR)
-		self.exp.display_frame(MAIN_FRAME,[FIXATION_LABEL])
 		
+		#Sound mixer initialization 
+		pg.mixer.init(frequency=22050, size=-16,channels=2, buffer=4096)
 	
-	
-	#########
+		# Creating left and right chanels
+		
 	def start_chunk(self, event):
 		self.play_sentence()
 	
@@ -53,47 +83,26 @@ class DichoticOneBack(object):
 	def play_sentence(self):
 		neu_sentence_sound_path = self.data_manager.sentence_inittial_path + '\\' + self.current_neu_sentence.file_path
 		neg_sentence_sound_path = self.data_manager.sentence_inittial_path + '\\' + self.current_neg_sentence.file_path
-		self.gui.after(0, lambda: playsound(neg_sentence_sound_path, block=False))
-		self.gui.after(0, lambda: playsound(neu_sentence_sound_path, block=False))
+		
+		left_channel = pg.mixer.Channel(0)
+		sound_left = pg.mixer.Sound(neu_sentence_sound_path)
+		#right_channel = pg.mixer.Channel(2)
+		#sound_right = pg.mixer.Sound(neg_sentence_sound_path)
+		
+		left_channel.play(sound_left)
+		left_channel.set_volume(0, 1.0)
+		
+		
+		
+		#self.gui.after(500, lambda: right_channel.play(sound_right))
+		#self.gui.after(500, lambda: 	right_channel.set_volume(0, 0))
+		
 		self.gui.after(int(self.current_neg_sentence.sentence_length)+300, self.next)
 		
 	def next(self):
 		print self.trial
 		self.next_trial()
 		self.play_sentence()
-	
-	## DUBLE EVRY THING TO NEG AND NEU
-	
-	
-	#########
-	
-	
-	def _play2tracks(self, event=None):
-		'''activating tow audio channels'''
-		try_sound = self.data_manager.sentence_inittial_path + '\\' + self.data_manager.sentences[0].file_path
-		pg.mixer.init(frequency=44000, size=-16,channels=2, buffer=4096)
-		
-		sound0 = pg.mixer.Sound(try_sound)
-		
-		channel0 = pg.mixer.Channel(0)
-		channel1 = pg.mixer.Channel(1)
-		channel2 = pg.mixer.Channel(2)
-
-		# Play the sound (that will reset the volume to the default).
-		channel0.play(sound0)
-		
-		# Now change the volume of the specific speakers.
-		# The first argument is the volume of the left speaker and
-		# the second argument is the volume of the right speaker.
-		channel0.set_volume(1.0, 0.0)
-		
-		def x():
-			channel1.play(sound0)
-			channel1.set_volume(0.0, 1.0)
-		self.gui.after(500, x)
-		self.gui.after(1000, lambda : channel2.play(sound0))
-
-	
 	 
 def main():
 	from ExGui import Experiment
@@ -154,8 +163,8 @@ def main():
 	dichotic_instance._create_task_main_frame()
 	gui.state('zoomed')# full screen with esc option
 	
+	gui.bind('<space>', dichotic_instance.dtd.start_chunk)
 	#gui.bind('<space>', dichotic_instance._play2tracks)
-	gui.bind('<space>', dichotic_instance.start_chunk)
 	exp.run()
 	
 	
