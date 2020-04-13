d71c195f485435e67b7b2cdf92f2773235a76d78
Tomer <tomer.oz22@gmail.com>
2020-04-13 10:20:09 +0300

    updates

--- Tasks/Data.py
+++ Tasks/Data.py
@@ -23,6 +23,7 @@ class SubjectData(object):
 		self.catch_trial_types = []
 		self.sentence_instances = [] # valence, text, path, duration etc....
 		self.experimental_phase = []
+		self.trials_phases = [] # practice or real trials
 		
 		# unpacked sentence properties:
 		self.sentences_valence = []
@@ -43,6 +44,7 @@ class SubjectData(object):
 		block = package.current_block
 		trial_type = self._classify_trial(package.is_catch_trial)
 		
+		
 		last_key = package.last_key_pressed # last key press
 		was_correct = package.last_trial_classification # was last trial correct or not
 		last_rt = package.last_RT # last trial rt
@@ -60,6 +62,9 @@ class SubjectData(object):
 		self.subject_col.append(self.subject)
 		self.gender_col.append(self.gender)
 		self.group_col.append(self.group)
+		self.trials_phases.append(package.current_trial_type_intance.trial_phase)
+		
+		
 		
 		if trial_type == 'catch':
 			self.catch_trial_types.append(package.correct)
@@ -68,7 +73,7 @@ class SubjectData(object):
 		
 		self.sentence_instances.append(sentence)
 		self.unpack_sentences_data(sentence)
-	
+			
 	def _classify_trial(self, is_catch_trial):
 		if is_catch_trial:
 			return 'catch'
@@ -94,7 +99,7 @@ class SubjectData(object):
 		columns = ['subject', 'trial num', 'experimental_phase', 'trial type', 'catch trial type',
 					'block', 'is correct', 'key pressed', 'RT', 'valence',
 					'text', 'duration', 'path', 'sentence num',
-					'num shown', 'gender', 'group']
+					'num shown', 'gender', 'group', 'trials_phases']
 					
 		rows = [
 					self.subject_col			,
@@ -114,6 +119,7 @@ class SubjectData(object):
 					self.nums_shown_types		,
 					self.gender_col				,
 					self.group_col				,
+					self.trials_phases			,
 				]
 				
 		for i,r in enumerate(rows):
